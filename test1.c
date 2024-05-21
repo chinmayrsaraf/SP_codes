96a0d39fd3159e651f6ad2cc731f9a24e6ae8516
Chinmay Saraf <98649870+chinmayrsaraf@users.noreply.github.com>
2024-05-22 03:02:03 +0530

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+extern int no1,no2,c[10];
+float f1,f2;
+char c1;
+int d1,d2;
+}
---
+++ test2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main()
+{
+int no1,no2,c[10];
+extern float f1,f2;
+char c3;
+}
